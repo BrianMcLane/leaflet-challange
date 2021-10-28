2561fc989e2f11dff7c97f34dae88a1178e8f3d7
Brian <brianmclane@Brians-MBP.hitronhub.home>
2021-10-28 14:18:58 -0500

    Popup, Legend

--- Leaflet-Step-1/Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/Leaflet-Step-1/static/js/logic.js
@@ -22,31 +22,50 @@ L.geoJSON(data, {
       var depth = feature.geometry.coordinates[2] // Finding depth
       var magnitude = feature.properties.mag*5  // Finding magnitude
       switch (true) {
-        case depth <= 1:
-          color = "green"
+        case depth > 50:
+          color = "#EA2C2C"
           break;
-        case depth <= 3:
-          color = "yellow"
+        case depth > 40:
+          color = "#EA822C"
           break;
-        case depth <= 5:
-          color = "orange"
+        case depth > 30:
+          color = "#EE9C00"
           break;
-          case depth <= 7:
-          color = "red"
+        case depth > 20:
+          color = "#EECC00"
           break;
+        case depth > 10:
+          color = "#D4EE00"
+          console.log(depth)
         default:
-          color = "blue"
+          color = "#98EE00"
           console.log(depth)
       }
 
       return L.circleMarker(latlng, {
         radius: magnitude, 
         fillColor: color,
-        fillOpacity: 0.5,
-        color: color 
-        });
+        fillOpacity: 0.75,
+        color: "black", 
+        weight: .5
+      });
+      
+    },
+    onEachFeature: function(feature, layer) {
+      var mag = feature.properties.mag
+      var depth = feature.geometry.coordinates[2]
+      var loc = feature.properties.place
+      layer.bindPopup(
+        "Magnitude: "
+          + mag
+          + "<br>Depth: "
+          + depth
+          + "<br>Location: "
+          + loc
+      );
     }
-  }).addTo(myMap);
+  })
+.addTo(myMap);
 })
 
 var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -58,12 +77,28 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
   accessToken: API_KEY
 }).addTo(myMap);
 
-var info = L.control({
+var legend = L.control({
   position: "bottomright"
 });
 
-info.onAdd = function() {
-  var div = L.DomUtil.create("div", "legend");
-  return div;
-};
-info.addTo(myMap)
+legend.onAdd = function() {    
+  var div = L.DomUtil.create("div", "info legend");
+    var grades = [-10, 10, 20, 30, 40, 50];    
+    var colors = [      
+      "#98EE00",      
+      "#D4EE00",      
+      "#EECC00",      
+      "#EE9C00",      
+      "#EA822C",      
+      "#EA2C2C"    
+    ];
+    // Looping through our intervals to generate a label with a colored square for each interval.    
+    for (var i = 0; i < grades.length; i++) {      
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> "      
+      + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");    
+    }    
+    return div;  
+  };
+  legend.addTo(myMap)
+
+  
